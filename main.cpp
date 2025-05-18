8cf2894bbd073def3464e2e1190a0f2dcdf29eeb
Swayfel <marcus.steuer.1@gmx.de>
2025-05-18 09:57:58 +0200

    #1 Add function to create and read from a txt file instead of hard coded arrays

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
 #include "bubblesort.h"
+
+const int NUMBER_OF_RANDOM_NUMBERS = 10000;
 
 void printArray(int* arr, int size);
 
+void createFileWithIntValues(int size);
+
 int main() {
 	std::cout << "Welcome to the sorting library!" << std::endl;
-	int sizeOfArray = 16;
-	int* arrayToBeSorted = new int[sizeOfArray]{ 28, 19, 2, 13, 10, 244, 219, 172, 290, 192, 20, 78, 29, 50, 68, 39 };
-	printArray(arrayToBeSorted, sizeOfArray);
 
-	int* sortedArray = bubbleSort(arrayToBeSorted, sizeOfArray);
-	printArray(sortedArray, sizeOfArray);
+	//createFileWithIntValues(NUMBER_OF_RANDOM_NUMBERS);
+	int* arrayToBeSorted = new int[NUMBER_OF_RANDOM_NUMBERS]{ };
+	std::ifstream myFile;
+	myFile.open("toBeSorted.txt");
+	std::string line;
+	int lineNumber = 0;
+	while (std::getline(myFile, line)){
+		arrayToBeSorted[lineNumber] = std::stoi(line);
+		lineNumber++;
+	}
+	lineNumber = 0;
+	myFile.close();
+	
+	printArray(arrayToBeSorted, NUMBER_OF_RANDOM_NUMBERS);
+
+	int* sortedArray = bubbleSort(arrayToBeSorted, NUMBER_OF_RANDOM_NUMBERS);
+	printArray(sortedArray, NUMBER_OF_RANDOM_NUMBERS);
 
 	delete[] arrayToBeSorted;
 	return 0;
@@ -22,4 +41,19 @@ void printArray(int* arr, int size) {
 		std::cout << arr[i] << " | ";
 	}
 	std::cout << std::endl;
+}
+
+void createFileWithIntValues(int size) {
+	std::ofstream myFile;
+	myFile.open("toBeSorted.txt");
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(1, 100000);
+	
+	int randomNumber = 0;
+	for (int i = 0; i < size; i++) {
+		randomNumber = dist(gen);
+		myFile << randomNumber << std::endl;
+	}
+	myFile.close();
 }
